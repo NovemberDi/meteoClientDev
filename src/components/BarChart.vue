1656b6b2188e28156c9309d07434c6c59df1516b
NovemberDi <khrustalev.d@yandex.ru>
2023-08-30 23:01:15 +0300

    Refact watcher queryResult for deep watch in finaly component <apexchart>

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -1,34 +1,36 @@
-<template>
-      <div id="chart" class="chart">
-        <apexchart ref="radar" type="area" height="350" :options="chartOptions" :series="series"></apexchart>
-      </div>
+<template >
+  <div id="chart" class="chart">
+    <apexchart @mouseMove="click" id="suka" ref="radar" type="area" height="350" :options="chartOptions" :series="series"></apexchart>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'BarChart',
-        props:{
-          chartOptions:{type:Object},
-          series:{type:Object},
-        },
-        data: function() {
-          return {      
-          }
-        },
-        watch:{
-          chartOptions:{
-            handler(newValue){
-            this.$refs.radar.updateOptions({
-        xaxis: {
-        categories: newValue.xaxis.categories
+export default {
+    name: 'BarChart',
+    props:{
+      chartOptions:{type:Object},
+      series:{type:Object},
+    },
+    data: function() {
+      return {      
       }
-});
-            console.log('PFKEGF')
-            },
-            deep: true
-          }
-        }  
-    }
+    },
+    methods:{
+    },
+    // watch:{
+    //   chartOptions:{
+    //     handler(newValue){
+    //       this.$refs.radar.updateOptions({
+    //           xaxis: {
+    //           categories: newValue.xaxis.categories
+    //           }
+    //       });
+    //       console.log('PFKEGF')
+    //     },
+    //     deep: true
+    //   }
+    // }  
+}
 </script>
 
 <style scoped>
--- src/components/HistoryCharts.vue
+++ src/components/HistoryCharts.vue
@@ -1,100 +1,112 @@
 <template>
-    <div class="historyChartsWrap">
-        <!-- <button @click="getData"> Жми</button> -->
-        <!-- <a v-for="item in queryResult" :key="item.content.timestamp">
-            {{ item.content.outTemp }}||{{ new Date(item.content.timestamp) }} 
-        </a> -->
-        <BarChart :chartOptions="chartOptions" :series="series"></BarChart>
-    </div>
+  <div class="historyChartsWrap">
+      <!-- <button @click="getData"> Жми</button> -->
+      <!-- <a v-for="item in queryResult" :key="item.content.timestamp">
+          {{ item.content.outTemp }}||{{ new Date(item.content.timestamp) }} 
+      </a> -->
+      <BarChart :chartOptions="chartOptions" :series="series"></BarChart>
+  </div>
 </template>
 
 <script>
-    export default {
-        data(){
-            return {
-                startTime: '',
-                endTime: '',
+  export default {
+      data(){
+          return {
+              startTime: '',
+              endTime: '',
 
-                series: [{
-            name: 'Улица',
-            data: [0]
-          }, {
-            name: 'Дом',
-            data: [0]
-          }],
-          chartOptions: {
-            chart: {
-              height: 350,
-              type: 'area'
-            },
-            dataLabels: {
-              enabled: false
-            },
-            stroke: {
-              curve: 'smooth'
-            },
-            xaxis: {
-              type: 'datetime',
-              categories: [new Date().toISOString()]
-            },
-            tooltip: {
-              x: {
-                format: 'dd.MM.yyyy HH:mm'
-              },
+              series: [{
+          name: 'Улица',
+          data: []
+        }, {
+          name: 'Дом',
+          data: []
+        }],
+        chartOptions: {
+          chart: {
+            height: 350,
+            type: 'area'
+          },
+          dataLabels: {
+            enabled: false
+          },
+          stroke: {
+            curve: 'smooth'
+          },
+          xaxis: {
+            type: 'datetime',
+            categories: [new Date().toISOString()],
+            tickPlacement: 'on'
+          },
+          tooltip: {
+            x: {
+              format: 'dd.MM.yyyy HH:mm'
             },
           },
+          noData: {
+            text: 'Загрузка..',
+          },
+          // chart: {
+          //   toolbar: {
+          //     show: true,
+          //     tools: {
+          //       pan: true,    
+          //     }
+          //   }
+          // }
+        },
+          }
+          
+      },
+      props:{
+          queryResult: {type: Object}
+      },
+      methods:{
+          getData(){
+            setTimeout(() => {
+              let point =  new Date();
+              this.startTime = point.setHours(point.getHours()-1024);
+              this.endTime = Date.now();
+              try{
+              this.$emit('onQuery',{startTime:this.startTime, endTime:this.endTime})
+            } catch{
+              this.getData()
             }
-            
-        },
-        props:{
-            queryResult: {type: Object}
-        },
-        methods:{
-            getData(){
-              
-                let point =  new Date();
-                this.startTime = point.setHours(point.getHours()-124);
-                this.endTime = Date.now();
-                try{
-                this.$emit('onQuery',{startTime:this.startTime, endTime:this.endTime})
-              } catch{
-                setTimeout(() => this.getData(), 500)
-              }
-            }
-        },
-        watch:{
-            queryResult(newValue){
-                this.chartOptions.xaxis.categories = newValue.map((item) => new Date(item.content.timestamp).toISOString());
-                setTimeout(() => {
-                  this.series[0].data = newValue.map((item) => -(-item.content.outTemp));
-                this.series[1].data = newValue.map((item) => -(-item.content.inTemp));
-                },100)
-                
-                
-                
-                
+          }, 500)
+          },
 
-            }
-        },
-        mounted(){
-          this.getData()
-        }
-    }
-       
+      },
+      watch:{
+          queryResult(newValue){
+              // this.chartOptions.xaxis.categories = newValue.map((item) => new Date(item.content.timestamp).toISOString());
+              this.chartOptions = { xaxis: {
+                  categories: newValue.map((item) => new Date(item.content.timestamp).toISOString())
+                  }}
+              setTimeout(() => {
+                this.series[0].data = newValue.map((item) => -(-item.content.outTemp));
+                this.series[1].data = newValue.map((item) => -(-item.content.inTemp));
+              },0)
+          }
+      },
+      mounted(){
+        this.getData()
+      }
+  }
+     
 </script>
 
 <style scoped>
 .historyChartsWrap{
-    display: flex;
-    align-items: center;
-    /* justify-content: center; */
-    flex-direction: column;
-    height: 60%;
-    color:#2f281f
+  display: flex;
+  align-items: center;
+  /* justify-content: center; */
+  flex-direction: column;
+  height: 60%;
+  color:#2f281f
 }
 .chartsMain{
-    height: 100%;
-    width: 100%;
-    
+  height: 100%;
+  width: 100%;
+  
 }
 </style>
