e9a6049819ad870551f7966456c9129ce8be60ff
NovemberDi <122225024+NovemberDi@users.noreply.github.com>
2024-05-07 19:09:02 +0300

    Update useConnect.js

--- src/hooks/useConnect.js
+++ src/hooks/useConnect.js
@@ -1,13 +1,1 @@
 // "use strict"
-// import { ref } from "vue";
-
-// export default function (demIn) {
-//     let demOut = ref();
-//     demOut.value = 'ЗАЛУПА'+demIn
-
-
-
-//     return {
-//         demOut,
-//       }
-// }
